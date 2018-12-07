d639ab4fb8bb5aef952cce9f79947f80f3b232cd
Nandan1981 <Nandan1981@gmail.com>
2018-12-07 17:33:09 +0530

    Changes to make the eureka service discoverable

--- src/main/java/com/eureka/auth/security/AuthenticationApplication.java
+++ src/main/java/com/eureka/auth/security/AuthenticationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EntityScan("com.eureka.auth.*")
 public class AuthenticationApplication {
 
     public static void main(String[] args) {
